8b66ad365813a4e3a785e8e42d9cff53799bd661
aLVINlEE9 <fldh3746@gmail.com>
2022-08-27 16:51:29 +0900

    Profile page done

--- react-social/src/components/rightbar/Rightbar.jsx
+++ react-social/src/components/rightbar/Rightbar.jsx
@@ -1,25 +1,82 @@
-import "./rightbar.css"
+import "./rightbar.css";
 import Online from "../online/Online";
 import { Users } from "../../dummyData";
 
-export default function Rightbar() {
-  return (
-	<div className="rightbar">
-    <div className="rightbarWrapper">
-      <div className="birthdayContainer">
-        <img src="/assets/gift.png" alt="" className="birthdayImg" />
-        <span className="birthdayText">
-          <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today
-        </span>
+export default function Rightbar({ profile }) {
+  const HomeRightbar = () => {
+    return (
+      <>
+        <div className="birthdayContainer">
+          <img src="/assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+        <img src="/assets/ad.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          {Users.map((u) => (
+            <Online key={u.id} user={u} />
+          ))}
+        </ul>
+      </>
+    );
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+      <h4 className="rightbarTitle">User information</h4>
+      <div className="righbarInfo">
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">City:</span>
+          <span className="rightbarInfoValue">New York</span>
+        </div>
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">From:</span>
+          <span className="rightbarInfoValue">Madrid</span>
+        </div>
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">Relationship:</span>
+          <span className="rightbarInfoValue">Single</span>
+        </div>
+        <h4 className="rightbarTitle">User information</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/person/3.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/person/4.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/person/5.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/person/6.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+        </div>
       </div>
-      <img src="/assets/ad.jpeg" alt="" className="rightbarAd" />
-      <h4 className="rightbarTitle">Online Friends</h4>
-      <ul className="rightbarFriendList">
-        {Users.map(u => (
-          <Online key={u.id} user={u} />
-        ))}
-      </ul>
+      </>
+      )
+  }
+
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <ProfileRightbar />
+      </div>
     </div>
-  </div>
-  )
+  );
 }
